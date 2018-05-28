ccf103f2dc71372d88d8e8ae8729d764196f65bc
twzjwang <twzjwang@gmail.com>
2018-05-28 17:01:46 +0800

    Add APIs to get data in specific date and all sensors latest data

---
+++ api/all_sensor_latest_data.py
@@ -0,0 +1,39 @@
+from config import HOST_URL
+from decode import split
+from decode import decode
+from uuid_list import get_uuid_list
+import requests
+import json
+import time
+
+def get_all_sensor_latest_data():
+    uuid_list = get_uuid_list()
+    all_latest_data = []
+    for uuid in uuid_list:        
+        payload = {'command':'get_all_claims', 'uuid':uuid}
+        response = requests.post(HOST_URL, json=payload)
+        claim_list =  response.json()
+        sensor_data = []
+        epoch = []
+        device_id = ''
+        for tx in claim_list:
+            payload = {'command':'get_claim_info', 'hash_txn':tx}
+            response = requests.post(HOST_URL, json=payload)
+            msg = split(response.text)[4]
+            sensor_data.extend(decode(msg)[0])
+            epoch.extend(decode(msg)[1])
+            device_id = split(response.text)[4].split('|')[4]
+        latest_data_epoch = 0
+        latest_data_string = ''
+        for i in range(0, len(epoch)):
+            if epoch[i] > latest_data_epoch:
+                latest_data_epoch = epoch[i]
+                latest_data_string = sensor_data[i]
+        if latest_data_string:
+            all_latest_data.append(latest_data_string)
+    all_latest_data_string = ''
+    for string in all_latest_data:
+        all_latest_data_string += string
+        all_latest_data_string += ','
+    json_string = '{"source":"all latest","feeds":[' + all_latest_data_string[0:len(all_latest_data_string)-1] + '],"version":"' + time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()) + '","num_of_records":' + str(len(all_latest_data)) + '}'
+    return json_string
---
+++ api/specific_sensor_date_data.py
@@ -0,0 +1,35 @@
+from config import HOST_URL
+from decode import split
+from decode import decode
+import requests
+import json
+import calendar
+import datetime
+import time
+
+def get_specific_sensor_date_data(uuid, date):
+    payload = {'command':'get_all_claims', 'uuid':uuid}
+    response = requests.post(HOST_URL, json=payload)
+    claim_list =  response.json()
+    sensor_data = []
+    epoch = []
+    device_id = ''
+    for tx in claim_list:
+        payload = {'command':'get_claim_info', 'hash_txn':tx}
+        response = requests.post(HOST_URL, json=payload)
+        msg = split(response.text)[4]
+        sensor_data.extend(decode(msg)[0])
+        epoch.extend(decode(msg)[1])
+        device_id = split(response.text)[4].split('|')[4]
+    epoch_a = time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
+    epoch_b = epoch_a + 86400
+    history_data = []
+    for i in range(0, len(epoch)):
+        if epoch[i] >= epoch_a and epoch[i] < epoch_b:
+            history_data.append(sensor_data[i])
+    sensor_data_string = ''
+    for string in history_data:
+        sensor_data_string += string
+        sensor_data_string += ','
+    json_string = '{"source":"date","feeds":[' + sensor_data_string[0:len(sensor_data_string)-1] + '],"version":"' + time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()) + '","num_of_records":' + str(len(history_data)) +', "device_id": "' + device_id + '"}'
+    return json_string
