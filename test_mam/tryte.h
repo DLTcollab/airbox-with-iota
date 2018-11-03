9cf28c367f3000f39f51251374f8c029c3cfc14b
ZanJun Wang <twzjwang@gmail.com>
2018-11-03 13:37:56 +0800

    Add MAM testing on RTL8195

---
+++ test_mam/tryte.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __COMMON_TRINARY_TRYTE_H_
+#define __COMMON_TRINARY_TRYTE_H_
+
+#include "stdint.h"
+
+typedef int8_t tryte_t;
+
+#endif
