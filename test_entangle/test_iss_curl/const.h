c36c1fda96dd2e1afacfe152efd3044674680b05
ZanJun Wang <twzjwang@gmail.com>
2018-11-13 14:01:15 +0800

    Add copyright on the changed files

--- test_entangle/test_iss_curl/const.h
+++ test_entangle/test_iss_curl/const.h
@@ -3,6 +3,8 @@
  * https://github.com/iotaledger/entangled
  *
  * Refer to the LICENSE file for licensing information
+ * 
+ * Modifications copyright (c) Zan-Jun Wang
  */
 
 #ifndef __COMMON_CURL_P_CONST_H
