5a22e3b4fc562b0bcb3ddd25dcb0d2c331a9a3b0
Wu Yu Wei <wusyong9104@gmail.com>
2019-02-16 21:57:22 +0800

    Add missing semicolon

--- main/config.h
+++ main/config.h
@@ -6,6 +6,6 @@ char pass[] = "";   // your network password
 char gps_lat[] = "";   // 你所在的緯度
 char gps_lon[] = "";  // 你所在的經度
 const int lass_period = 60; // 幾秒想傳一次資料 建議實際上線是 60
-uint8_t iota_node[4] = {140, 116, 246, 164}
+uint8_t iota_node[4] = {140, 116, 246, 164};
 
 #endif
